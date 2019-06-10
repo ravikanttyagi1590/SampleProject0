ac90f12a82762d37de812d3ffd82f66d5b559355
ravikanttyagi1590 <49584574+ravikanttyagi1590@users.noreply.github.com>
2019-06-10 10:49:29 +0530

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,11 @@
+class Calc
+{
+  public static void main(String args[])
+  {
+    int a,b;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    System.out.println(a+b);
+  }
+}
