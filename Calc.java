084a873a93009da3787baa4aca9d2e273bf3d58c
ravikanttyagi1590 <49584574+ravikanttyagi1590@users.noreply.github.com>
2019-06-10 11:57:40 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 class Calc
 {
   public static void main(String args[])
   {
     int a,b;
-    Scanner s=new Scanner(System.in);
-    a=s.nextInt();
-    b=s.nextInt();
+    //Scanner s=new Scanner(System.in);
+    a=Integer.parseInt(args[1]);
+    b=Integer.parseInt(args[2])
     System.out.println(a+b);
   }
 }
